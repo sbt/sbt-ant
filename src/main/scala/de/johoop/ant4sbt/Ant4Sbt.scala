5ed18f570729d5aa6da7455bb2e5bc3ee9792d63
Joachim Hofer <jmhofer.github@johoop.de>
2012-03-09 15:20:46 +0100

    initial commit

---
+++ src/main/scala/de/johoop/ant4sbt/Ant4Sbt.scala
@@ -0,0 +1,27 @@
+/*
+ * This file is part of ant4sbt.
+ *
+ * Copyright (c) 2012 Joachim Hofer
+ * All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package de.johoop.ant4sbt
+
+import sbt._
+import sbt.Keys._
+
+object Ant4Sbt extends Plugin with Settings {
+
+  val antSettings = Seq(
+      antBuildFile := file("build.xml"),
+      ant <<= (antBuildFile, streams) map antTask)
+
+  def antTask(buildFile: File, streams: TaskStreams): Unit = {
+    streams.log.debug("Executing ant task for build file '%s'" format buildFile.getName)
+  }
+}
+
